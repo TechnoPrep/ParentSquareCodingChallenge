05fcf07aec9e727d8bec9825f3ee28de4e06f305
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2022-07-02 12:04:59 -0600

    refinded logic, completed in 3 steps, not the most optimal

--- challenges/MissingParents.js
+++ challenges/MissingParents.js
@@ -7,38 +7,34 @@ const { studentsArr, parentsArr } = require('../index');
  * @returns [ {} ]
  */
 const missingParentContact = (studentArr, parentArr) => {
-  //List of Stu Ids
-  let stuData = studentArr.map(stu => stu.student_id);
+  //Return an Array of student with no parents
+  let noPar = studentsArr.filter((stu) =>{
+    return !parentsArr.some((par) => {
+      return stu.student_id === par.student_id
+    })
+  }) 
 
-  //List of Students Parents
-  let Par = parentArr.map(par => par.student_id)
-
-  //List of Parents with no Contact Info
-  let parNoCon = parentArr.map((par) => {
-    if(par.email === '' && par.mobile === ''){
-      return par.student_id
-    }
-  }).filter(x => x != undefined);  
-
-  //List of Students with no Matching Parent
-  let noMatch = stuData.map((stu) => {
-    if(Par.indexOf(stu) === -1){
-      return stu
-    }
-  }).filter(x => x != undefined);  
-
-  // Unique Array of Student Id's
-  let stuFinal = [...new Set([...parNoCon, ...noMatch])];
-
-  return studentArr.map((stu) => {
-    if(stuFinal.includes(stu.student_id)){
-      return { 
-        student_id: stu.student_id, 
-        first_name: stu.FirstName, 
-        last_name: stu.LastName 
+  //Return an array of students with parents, but no contact info
+  let noInfo = studentsArr.filter((stu) =>{
+    return parentsArr.some((par) => {
+      if(stu.student_id === par.student_id && (par.email === '' && par.mobile === '')){
+        return {student_id: stu.student_id}
       }
+    })
+    
+  }) 
+
+  //Combine Array
+  let results = [...new Set([...noPar, ...noInfo])]
+
+  //Return only Student_id, Firstname, LastName fields
+  return results.map((stu) => {
+    return {
+      student_id: stu.student_id,
+      first_name: stu.FirstName,
+      last_name: stu.LastName
     }
-  }).filter(x => x != undefined);
+  })
 
 }
 
