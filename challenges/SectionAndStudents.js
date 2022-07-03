e3c8a258bfc1e784b6022b8bf58c03f69e94dd72
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2022-07-03 12:43:59 -0600

    updated logic to be more efficient and use less memory

--- challenges/SectionAndStudents.js
+++ challenges/SectionAndStudents.js
@@ -6,36 +6,68 @@ const { rostersArr, sectionsArr } = require('../index');
  * @param {*} sectionsArr 
  * @returns [ {} ]
  */
+// const secAndStu = (rostersArr, sectionsArr) => {
+
+//   let secArr = []
+
+//   //Iterate through the SectionArr
+//   for (let i = 0; i < sectionsArr.length; i++) {
+//     const section = sectionsArr[i].section_id
+//     const course = sectionsArr[i].course_name
+//     const students = []
+
+//     if(section != undefined){
+//       //Iterate throught rosterArr
+//       for (let j = 0; j < rostersArr.length; j++) {
+//         //if match, push student_id to array
+//         if(section === rostersArr[j].section_id){
+//           students.push(rostersArr[j].student_id)
+//         }
+//       }
+    
+//       let secObj = {
+//         section_id: section,
+//         course_name: course,
+//         student_ids: students,
+//       }
+      
+//       secArr.push(secObj)
+//     }
+//   }
+
+//   return secArr
+
+// }
+
 const secAndStu = (rostersArr, sectionsArr) => {
 
-  let secArr = []
-
-  //Iterate through the SectionArr
-  for (let i = 0; i < sectionsArr.length; i++) {
-    const section = sectionsArr[i].section_id
-    const course = sectionsArr[i].course_name
-    const students = []
-
-    if(section != undefined){
-      //Iterate throught rosterArr
-      for (let j = 0; j < rostersArr.length; j++) {
-        //if match, push student_id to array
-        if(section === rostersArr[j].section_id){
-          students.push(rostersArr[j].student_id)
+  rostersArr.map((ros) => {
+    sectionsArr.map((sec) => {
+      //check if ros and section_id match
+      if(ros.section_id === sec.section_id){
+        //If matched, check if the obj had the students property
+        if(!sec.hasOwnProperty('students')){
+          //If not, create the property
+          sec.students = []
         }
+        //push student to students array in section obj
+        sec.students.push(ros.student_id) 
       }
-    
-      let secObj = {
-        section_id: section,
-        course_name: course,
-        student_ids: students,
-      }
-      
-      secArr.push(secObj)
-    }
-  }
+    })
+  })
 
-  return secArr
+  // Filter out any sections with a section_id, and only return required properties
+  return sectionsArr.filter((sec) =>{
+    if(sec.hasOwnProperty('section_id')){
+      return sec
+    }
+  }).map((sec)=>{
+    return{
+      section_id: sec.section_id,
+      course_name: sec.course_name,
+      students: sec.students
+    }
+  })
 
 }
 
