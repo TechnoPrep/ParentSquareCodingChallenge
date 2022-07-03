32dcdd2db0feaff7b71d581ca2b6fd79185f60e2
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2022-07-03 12:44:33 -0600

    removed commented code

--- challenges/SectionAndStudents.js
+++ challenges/SectionAndStudents.js
@@ -6,39 +6,6 @@ const { rostersArr, sectionsArr } = require('../index');
  * @param {*} sectionsArr 
  * @returns [ {} ]
  */
-// const secAndStu = (rostersArr, sectionsArr) => {
-
-//   let secArr = []
-
-//   //Iterate through the SectionArr
-//   for (let i = 0; i < sectionsArr.length; i++) {
-//     const section = sectionsArr[i].section_id
-//     const course = sectionsArr[i].course_name
-//     const students = []
-
-//     if(section != undefined){
-//       //Iterate throught rosterArr
-//       for (let j = 0; j < rostersArr.length; j++) {
-//         //if match, push student_id to array
-//         if(section === rostersArr[j].section_id){
-//           students.push(rostersArr[j].student_id)
-//         }
-//       }
-    
-//       let secObj = {
-//         section_id: section,
-//         course_name: course,
-//         student_ids: students,
-//       }
-      
-//       secArr.push(secObj)
-//     }
-//   }
-
-//   return secArr
-
-// }
-
 const secAndStu = (rostersArr, sectionsArr) => {
 
   rostersArr.map((ros) => {
